93bf00549e4ea7a5c8fc569712e2b00c21ccde9b
artketler <artketler.ak@gmail.com>
2023-01-29 23:06:53 +0300

    Exercise 023 project

---
+++ Exercise023/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+bool thisNumber = false;
+int N = 0;
+
+while (!thisNumber)
+{
+  Console.Write("Введите число, для получения таблицы кубов чисел от 1 до Вашего числа: ");
+  thisNumber = int.TryParse(Console.ReadLine(), out N);
+}
+
+Console.Write($"{N} -> ");
+if (N == 0)
+{
+  Console.Write("0\n");
+}
+else
+{
+  int i = 0;
+  while ((N > 0 && i < N) || (N < 0 && i > N))
+  {
+    i = N > 0 ? i + 1 : i - 1;
+    double cubeN = Math.Pow(i, 3);
+    Console.Write(cubeN + (Math.Abs(i) < Math.Abs(N) ? ", " : "\n"));
+  }
+}
