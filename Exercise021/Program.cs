3f18dab38635f28156da56f8a1677d8372932286
artketler <artketler.ak@gmail.com>
2023-01-29 22:50:16 +0300

    Exercise 021 project

--- Exercise021/Program.cs
+++ Exercise021/Program.cs
@@ -2,19 +2,7 @@
 
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
-Console.WriteLine("Найдём расстояние между двумя точками в 3D пространстве:");
-int x1 = getMean("A", "x");
-int y1 = getMean("A", "y");
-int z1 = getMean("A", "z");
 
-int x2 = getMean("B", "x");
-int y2 = getMean("B", "y");
-int z2 = getMean("B", "z");
-
-double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-System.Console.WriteLine($"A ({x1}, {y1}, {z1}); B ({x2}, {y2}, {z2}) -> расстояние между ними в 3D пространстве ({Math.Round(dist, 2)})");
-
-// --- FUNCs --- //
 int getMean(string pointName, string coordName)
 {
   int pointSize = 0;
@@ -27,3 +15,16 @@ int getMean(string pointName, string coordName)
   }
   return pointSize;
 }
+
+Console.WriteLine("Найдём расстояние между двумя точками в 3D пространстве:");
+int x1 = getMean("A", "x");
+int y1 = getMean("A", "y");
+int z1 = getMean("A", "z");
+
+int x2 = getMean("B", "x");
+int y2 = getMean("B", "y");
+int z2 = getMean("B", "z");
+
+double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+System.Console.WriteLine($"A ({x1}, {y1}, {z1}); B ({x2}, {y2}, {z2}) -> расстояние между ними в 3D пространстве ({Math.Round(dist, 2)})");
+
